1924dcdc4513c2e11cfa55a0515f6b50e325b690
who1997 <1397400733@qq.com>
2018-05-16 18:23:08 +0800

    Delete record.js

--- homework/homework/js/record.js
+++
@@ -1,7 +0,0 @@
-function record_XY(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    var p = document.getElementById("dis");
-    var txt = p.textContent;
-    p.innerHTML = txt + ' ' + x + ',' + y;
-}
